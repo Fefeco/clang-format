eb10c888eea076c53c7dd325fc4b9216f2c03df8
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2025-04-03 11:29:58 +0200

    Automatice building process

--- script.sh
+++ script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+CONTAINER="clang-format"
+
 # Verifica si se han pasado argumentos
 if [ "$#" -lt 1 ]; then
   echo "No arguments provided"
   exit 1
 fi
 
+# Verifica si existe el contenedor
+if [ $(docker images -aqf reference=$CONTAINER | wc -l) -eq 0 ]; then
+  echo "Building $CONTAINER container"
+  docker build -t clang-format ~/clang
+fi
+
 # Ejecuta el contenedor Docker con los argumentos pasados
-docker run --rm -v $(pwd):/usr/src/app clang-format bash -c "clang-format $*"
+docker run --rm -v $(pwd):/usr/src/app clang-format \
+bash -c "clang-format $*"
